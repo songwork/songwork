23028c18e5adaf5839009e42ca7a37df6a4bb64a
Songwork <songwork@songwork.com>
2010-05-20 19:04:00 -0700

    template1

--- test/regen-fixtures.sh
+++ test/regen-fixtures.sh
@@ -1,5 +1,5 @@
 # create NEW test fixtures 
 dropdb songwork_test
-createdb -O songwork -E UTF8 songwork_test
+createdb -T template1 -O songwork -E UTF8 songwork_test
 psql -q -X -U songwork -d songwork_test -f schema.pgsql 
 psql -q -X -U songwork -d songwork_test -f fixtures.pgsql 
--- test/regen-scratch.sh
+++ test/regen-scratch.sh
@@ -1,6 +1,6 @@
-createdb -U songwork -O songwork -E UTF8 songwork
+createdb -T template1 -U songwork -O songwork -E UTF8 songwork
 psql -X -U songwork -d songwork -f schema.pgsql 
 
-createdb -U songwork -O songwork -E UTF8 songwork_test
+createdb -T template1 -U songwork -O songwork -E UTF8 songwork_test
 psql -X -U songwork -d songwork_test -f schema.pgsql 
 psql -X -U songwork -d songwork_test -f fixtures.pgsql 
