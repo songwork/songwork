f3f6fbac83047fe078b30bf81c4c219e32f05401
Songwork <songwork@songwork.com>
2010-05-20 19:05:49 -0700

    template0

--- test/regen-scratch.sh
+++ test/regen-scratch.sh
@@ -1,6 +1,6 @@
-createdb -T template1 -U songwork -O songwork -E UTF8 songwork
+createdb -T template0 -U songwork -O songwork -E UTF8 songwork
 psql -X -U songwork -d songwork -f schema.pgsql 
 
-createdb -T template1 -U songwork -O songwork -E UTF8 songwork_test
+createdb -T template0 -U songwork -O songwork -E UTF8 songwork_test
 psql -X -U songwork -d songwork_test -f schema.pgsql 
 psql -X -U songwork -d songwork_test -f fixtures.pgsql 
